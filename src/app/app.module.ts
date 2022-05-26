1131b0df7a30cd659d06797ad46a2ff26ab385b6
ale94 <alejandrorua111@gmail.com>
2022-05-26 00:13:40 -0300

    school, skills, projects and footer components creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { BannerComponent } from './components/banner/banner.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { SocialComponent } from './components/social/social.component';
 import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SchoolComponent } from './components/school/school.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,12 @@ import { AboutComponent } from './components/about/about.component';
     BannerComponent,
     LogoComponent,
     SocialComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienceComponent,
+    SchoolComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
