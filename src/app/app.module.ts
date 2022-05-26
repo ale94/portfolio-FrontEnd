87518efb071265f31e42fcc9ad246477af0a057c
ale94 <alejandrorua111@gmail.com>
2022-05-25 23:33:19 -0300

    header component render

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    BannerComponent,
+    LogoComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
